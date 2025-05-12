25ccb02f9d07226c7358033b0e0829056bc34fce
Kirill <forwork.prosto10ia@gmail.com>
2025-05-12 14:19:57 +1000

    поправил немного css

--- src/components/InteractiveSection.jsx
+++ src/components/InteractiveSection.jsx
@@ -5,13 +5,13 @@ export default function InteractiveSection() {
   return (
     <section className="section-interactive">
       <div className="interactive-container">
-        <h2 className="interactive-title">ИНТЕРАКТИВ</h2>
-        <div className="interactive-content">
-          <p className="interactive-text">
-            Твой рекорд начинается здесь<br/>– проверь свою скорость!
-          </p>
+         <img className="img-interactive" src={cubeInteractive} alt="Интерактивный кубик" loading="lazy"/>
+         <h2 className="interactive-title">ИНТЕРАКТИВ</h2>
+         <div className="interactive-content">
+            <p className="interactive-text">
+               Твой рекорд начинается здесь<br/>– проверь свою скорость!
+            </p>
         </div>
-        <img className="img-interactive" src={cubeInteractive} alt="Интерактивный кубик" loading="lazy"/>
       </div>
     </section>
   );
